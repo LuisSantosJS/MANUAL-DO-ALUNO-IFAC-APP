adb02ad6dc6ee2eaf5c08702876c5b244463ab13
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-21 14:03:47 -0500

    finish parcial

--- src/pages/Wellcome/styles.ts
+++ src/pages/Wellcome/styles.ts
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         backgroundColor: '#FFFFFF'
     },
     gridCollumm: {
