0f6dd6e29ec39315dc0e7e9fe9dfd2a2bbc58e98
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-09-21 15:50:11 -0500

    ajustes css

--- src/pages/Wellcome/index.tsx
+++ src/pages/Wellcome/index.tsx
@@ -27,7 +27,7 @@ const Wellcome: React.FC = () => {
         <>
             <View style={{ width: '100%', backgroundColor: 'white', height: getStatusBarHeight(true) }} />
             <View style={styles.container}>
-                <View style={[styles.gridCollumm, { height:  Platform.OS == 'ios' ?'20%' : '12%', top: Platform.OS == 'android' ? 12 : 0 }]}>
+                <View style={[styles.gridCollumm, { height:  Platform.OS == 'ios' ?'20%' : '15%', top: Platform.OS == 'android' ? 12 : 0 }]}>
                     <Image style={styles.imageContainer} resizeMode={'contain'} source={IfacLogo} />
                     <View style={styles.betaView}>
                         <Text style={styles.betaText}>BETA 1.0</Text>
